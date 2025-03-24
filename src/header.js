5608940b33d4cc53587d5cc7334259b20c804986
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2025-03-25 00:27:15 +0300

    feat: fih some bugs

--- src/header.js
+++ src/header.js
@@ -1,5 +1,19 @@
 const burgerToggle = document.getElementById("burger-toggle");
 const menuLinks = document.querySelectorAll("nav ul li a");
+const navigateToConstructorButton = document.getElementById(
+  "navigate-to-constructor-button"
+);
+
+navigateToConstructorButton.addEventListener("click", () => {
+  const target = document.getElementById("constructor");
+
+  // Плавная прокрутка с дополнительными настройками
+  target.scrollIntoView({
+    behavior: "smooth", // анимация прокрутки
+    block: "start", // вертикальное выравнивание (start/center/end/nearest)
+    inline: "nearest", // горизонтальное выравнивание
+  });
+});
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
